ec765bd9040a8775e4af20d25f45a5b6b9df1bb8
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-04-08 18:43:53 +0200

    realized section statistics with svg

--- src/components/main/Courses.vue
+++ src/components/main/Courses.vue
@@ -7,7 +7,7 @@
             <div class="courses-box-info shadow position-relative text-center">
               <div class="new-corner-img position-absolute"></div>
               <h1 class="mb-5">Courses</h1>
-              <p class="mb-5">
+              <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                 ultrices auctor sapien id cursus. Aliquam maximus turpis in
                 vehicula semper.
@@ -17,11 +17,64 @@
               </button>
             </div>
           </div>
-          <div class="col-7 ms-0 d-flex h-100 py-5 px-0 justify-content-between">
-            <div v-for="(course, index) in courses" :key="index" class="course-name-info text-center py-4">
-              <img :src="course.imgUrl" alt="car image" class="mb-3">
-              <h2 class="mb-3">{{course.name}}</h2>
-              <button class="my-btn-grey text-uppercase rounded-pill">learn more</button>
+          <div
+            class="col-7 ms-0 d-flex h-100 py-5 px-0 justify-content-between"
+          >
+            <div
+              v-for="(course, index) in courses"
+              :key="index"
+              class="course-name-info text-center py-4"
+            >
+              <img :src="course.imgUrl" alt="car image" class="mb-3" />
+              <h2 class="mb-3">{{ course.name }}</h2>
+              <button class="my-btn-grey text-uppercase rounded-pill">
+                learn more
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="statistics position-relative">
+      <div class="container-fluid justify-content-center h-100">
+        <div class="row h-100">
+          <div class="col-12 d-flex justify-content-center">
+            <div class="separator position-absolute"></div>
+          </div>
+          <div class="col 12 wrapper-statistics-cards position-relative h-100">
+            <div class="container px-5 h-100">
+              <div class="row px-5 h100 text-center">
+                <div class="col-4">
+                  <div class="statistics-card shadow position-relative">
+                    <svg>
+                      <circle class="bg" cx="100" cy="100" r="95" />
+                      <circle class="meter-1" cx="100" cy="100" r="95" />
+                    </svg>
+                    <p class="position-absolute">95%</p>
+                    <h5 class="text-uppercase">pass rate</h5>
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="statistics-card shadow position-relative">
+                    <svg>
+                      <circle class="bg" cx="100" cy="100" r="95" />
+                      <circle class="meter-2" cx="100" cy="100" r="95" />
+                    </svg>
+                    <p class="position-absolute">100%</p>
+                    <h5 class="text-uppercase">referral rate</h5>
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="statistics-card shadow position-relative">
+                    <svg>
+                      <circle class="bg" cx="100" cy="100" r="95" />
+                    </svg>
+                    <p class="position-absolute">0%</p>
+                    <h5 class="text-uppercase">accident rate</h5>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -33,25 +86,24 @@
 <script>
 export default {
   name: "MainCourses",
-  data: function (){
-      return {
-        courses: [
-          {
-            name: "Pass Plus",
-            imgUrl : require('@/assets/images/courses-passplus-200x200.jpg')
-          },
-            {
-            name: "Intensive Course",
-            imgUrl : require('@/assets/images/course-intensive-200x200.jpg')
-          },
-            {
-            name: "Instructors",
-            imgUrl : require('@/assets/images/courses-instructor-200x200.jpg')
-          },
-        ]
-      }
-    
-  }
+  data: function () {
+    return {
+      courses: [
+        {
+          name: "Pass Plus",
+          imgUrl: require("@/assets/images/courses-passplus-200x200.jpg"),
+        },
+        {
+          name: "Intensive Course",
+          imgUrl: require("@/assets/images/course-intensive-200x200.jpg"),
+        },
+        {
+          name: "Instructors",
+          imgUrl: require("@/assets/images/courses-instructor-200x200.jpg"),
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -59,6 +111,7 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/scss/partials/_variables";
 @import "@/assets/scss/partials/_common";
+@import "~@fortawesome/fontawesome-free/css/all.css";
 
 section.courses {
   height: 520px;
@@ -90,17 +143,83 @@ section.courses {
       font-size: 1.04rem;
       line-height: 2.1rem;
       font-weight: 400;
+      margin-bottom: 2rem;
     }
   }
-  div.course-name-info{
-    h2{
+  div.course-name-info {
+    h2 {
       color: $secondaryColor;
       font-size: 1.6rem;
       font-weight: 600;
     }
-    button{
-      font-size: .7rem;
+    button {
+      font-size: 0.7rem;
     }
+  }
+}
+
+section.statistics {
+  height: 520px;
+  div.separator {
+    height: 500px;
+    width: 150%;
+    border-radius: 120%;
+    background-color: rgb(228, 233, 237);
+    bottom: 450px;
+    z-index: -1;
+  }
+  div.wrapper-statistics-cards {
+    bottom: 1.5rem;
+    z-index: 2;
+    div.statistics-card {
+      background-color: white;
+      border-radius: 12px;
+      border-top: 6px solid $mainColor;
+      padding: 3.5rem 2.5rem;
+    }
+    h5 {
+      color: rgb(147, 155, 163);
+      font-weight: 600;
+    }
+    p{
+      bottom:190px;
+      left: 50%;
+      transform: translateX(-50%);
+      font-weight: 600;
+      font-size: 2.6rem;
+      color: rgb(147, 155, 163);
+    }
+    .col-4{
+      padding: 0 .3rem;
+    }
+  }
+  svg {
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+    margin-bottom: 3rem;
+  }
+  .bg {
+    fill: none;
+    stroke-width: 10px;
+    stroke: $lightGrayBg;
+  }
+  [class^="meter-"] {
+    fill: none;
+    stroke-width: 10px;
+    stroke-linecap: round;
+    transform: rotate(-90deg);
+    transform-origin: 50% 50%;
+  }
+  .meter-1 {
+    stroke-dasharray: 560;
+    stroke-dashoffset: 0;
+    stroke: $mainColor;
+  }
+  .meter-2 {
+    stroke-dasharray: 0;
+    stroke-dashoffset: 0;
+    stroke: $mainColor;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faCircleNotch, faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
 library.add(faPhoneFlip)
+library.add(faCircleNotch)
+
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
